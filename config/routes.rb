1775ee8bdc9aae74c466bc008c1657b548774372
edubs5 <ebdubbin@gmail.com>
2015-12-10 20:44:00 -0800

    Create routes for oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'auth/failure', to: redirect('/')
 end
