e9cdd36543b45634ac6d55cc1e2dea194eec9dcf
edubs5 <ebdubbin@gmail.com>
2015-12-14 21:48:02 -0800

    Update User Spec to use mock_auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,28 +3,19 @@ require "rails_helper"
 RSpec.describe User, :type => :model do
   describe "when looking for a user" do
     before(:example) do
-      @auth = {
-        "provider": "auth_provider",
-        "uid":      "00001",
-        "info": {
-          "name":       "Hello World",
-          "email":      "hello_world@example.com",
-          "first_name": "Hello",
-          "last_name":  "World"
-        }
-      }
+      @mock_auth = OmniAuth.config.mock_auth[:google_oauth2]
     end
 
     describe ".from_omniauth" do
       it "returns a user" do
-        user = User.from_omniauth(@auth)
+        user = User.from_omniauth(@mock_auth)
         expect(user).to_not be nil
       end
     end
 
     describe ".create_from_omniauth" do
       it "creates a user" do
-        expect{ User.create_from_omniauth(@auth) }
+        expect{ User.create_from_omniauth(@mock_auth) }
           .to change{ User.count }
           .by 1
       end
