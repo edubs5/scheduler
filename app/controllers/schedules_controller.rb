89614457f0d2c8e03e9853175b317469056d2313
edubs5 <ebdubbin@gmail.com>
2015-12-21 23:34:41 -0800

    Create Schedules #index view, redirect to schedules_path after login

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,5 @@
+class SchedulesController < ApplicationController
+  def index
+    @schedules = current_user.schedules
+  end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in!"
+    redirect_to schedules_path, notice: "Signed in!"
   end
 
   def destroy
