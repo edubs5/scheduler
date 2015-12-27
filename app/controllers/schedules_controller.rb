7d086295a157af3af48178317a657594b0f32640
edubs5 <ebdubbin@gmail.com>
2015-12-26 23:35:21 -0800

    Add to Schedule Create and Delete methods

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,28 @@
 class SchedulesController < ApplicationController
   def index
     @schedules = current_user.schedules
+    @locations = current_user.locations
+  end
+
+  def create
+    @schedule = current_user.schedules.new(schedule_params)
+    if @schedule.save
+      redirect_to schedules_path, alert: "Schedule created successfully."
+    else
+      redirect_to schedules_path, alert: "Unable to create schedule."
+    end
+  end
+
+  def destroy
+    schedule = Schedule.find_by_id(params[:id])
+    schedule.destroy
+    redirect_to schedules_path, alert: "Schedule deleted."
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:name, :start_date, :end_date, :user_id,
+      :location_id)
   end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,4 @@
 class Schedule < ActiveRecord::Base
   belongs_to :user
-  validates :start_date, :end_date, :user, presence: true
+  validates :start_date, :end_date, :user_id, presence: true
 end
