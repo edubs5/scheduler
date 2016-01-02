442f641867f51e7f005d96aa2af227c3ec0a61fe
edubs5 <ebdubbin@gmail.com>
2016-01-01 22:55:37 -0800

    Update Schedule with friendly_id based on name, location. Update slug on name change

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,7 @@
 class Schedule < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :slug_candidates, use: [:slugged, :history]
+
   validates :start_date, :end_date, :user_id, :name, presence: true
 
   belongs_to :user
@@ -12,5 +15,21 @@ class Schedule < ActiveRecord::Base
     start_date.upto(end_date) do |date|
       Workday.create(schedule_id: id, work_date: date)
     end
+  end
+
+  def slug_candidates
+    [
+      :name,
+      [:name, location.name],
+      [:name, location.name, random_string]
+    ]
+  end
+
+  def random_string
+    SecureRandom.urlsafe_base64(10)
+  end
+
+  def should_generate_new_friendly_id?
+    name_changed? || super
   end
 end
