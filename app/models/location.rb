9a5b1f7ee0a148a054d556f9bbc641f99762a241
edubs5 <ebdubbin@gmail.com>
2016-02-27 16:15:34 -0800

    Create Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,22 @@
 class Location < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :slug_candidates, use: [:slugged, :history]
+
   belongs_to :user
   validates :name, :user, presence: true
+
+  def slug_candidates
+    [
+      :name,
+      [:name, random_string]
+    ]
+  end
+
+  def random_string
+    SecureRandom.urlsafe_base64(10)
+  end
+
+  def should_generate_new_friendly_id?
+    name_changed? || super
+  end
 end
