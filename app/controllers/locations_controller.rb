1f1b38086725f77f9bcdf7f6cdb7517cddde1ff4
edubs5 <ebdubbin@gmail.com>
2016-02-27 16:15:15 -0800

    Create Locations Controller, and views

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,45 @@
+class LocationsController < ApplicationController
+  def index
+    @locations = current_user.locations
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @locations = current_user.locations
+    params["location"]["user_id"] = current_user.id
+    @location = Location.create(location_params)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def update
+    @locations = current_user.locations
+    @location = Location.friendly.find(params[:id])
+    @location.update_attributes(location_params)
+  end
+
+  def edit
+    @location = Location.friendly.find(params[:id])
+    @locations = current_user.locations
+  end
+
+  def destroy
+    location = Location.find_by_id(params[:id])
+    location.destroy
+    flash[:danger] = "Location deleted."
+    redirect_to locations_path
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(
+      :name,
+      :user_id
+    )
+  end
+end
